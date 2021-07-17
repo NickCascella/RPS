1248138e4a78ece8e63c5f3acd33c15aed6c862e
WhatAmIDoingWrong98 <83091530+WhatAmIDoingWrong98@users.noreply.github.com>
2021-07-17 13:17:24 -0400

    Add files via upload

--- rps.js
+++ rps.js
@@ -1,9 +1,12 @@
 let computerScore = 0;
 let humanScore = 0;
 
+
+
+
 //computer AI
 function computerPlay() {
-     return Math.floor(1 + Math.random()*3)
+     return Math.floor(1 + Math.random()*3)    
 };
 let computerSelection = computerPlay();
 
@@ -15,69 +18,137 @@ if (computerSelection == 1) {
 } else if (computerSelection == 3) {
     computerSelection = "Scissors"
 } 
-
+//score keeping function 
 
 //setting possible outcomes
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.length === computerSelection.length){
-        console.log("It's a tie!"); 
+        comments.textContent = 'You both are no good!'; 
     } else if (playerSelection.length == 4 && computerSelection.length === 5) {
-        console.log("You lose! Paper beats Rock!");   
+        comments.textContent = "Damn! Paper beats Rock! :(";
         computerScore++
     } else if (playerSelection.length == 4 && computerSelection.length === 8) {
-        console.log("You win! Rock beats Scissors!");   
+        comments.textContent = "Nice! Rock beats Scissors!";   
         humanScore++
     } else if (playerSelection.length == 5 && computerSelection.length === 4) {
-        console.log("You win! Paper beats Rock!");  
+        comments.textContent = "Good going! Paper beats Rock!";  
         humanScore++
     }else if (playerSelection.length == 5 && computerSelection.length === 8) {
-        console.log("You lose! Scissors beats Paper!"); 
+        comments.textContent = "Try harder! Scissors beats Paper!"; 
         computerScore++
     }else if (playerSelection.length == 8 && computerSelection.length === 4) {
-        console.log("You lose! Rock beats Scissors!");  
+        comments.textContent = "Damn! Rock beats Scissors!";  
         computerScore++
     } else if (playerSelection.length == 8 && computerSelection.length === 5) {
-        console.log("You win! Scissors beats Paper!");   
+        comments.textContent = "Yay! Scissors beats Paper!";   
         humanScore++
     } else {
-        console.log("Something went wrong!");
+        comments.textContent = 'Nice going. something is broken >:(';
 }
 }
 
-function game(){
 
-computerSelection = computerPlay();
+let decisionR = () =>{
+    let playerSelection = 'rock'
+    computerSelection = computerPlay()
 
-//setting random number to a choice
-if (computerSelection == 1) {
-     computerSelection = 'Rock';
-} else if (computerSelection == 2) {
-    computerSelection = "Paper"
-} else if (computerSelection == 3) {
-    computerSelection = "Scissors"
-} 
+    
+    if (computerSelection == 1) {
+        computerSelection = 'Rock';
+   } else if (computerSelection == 2) {
+       computerSelection = "Paper"
+   } else if (computerSelection == 3) {
+       computerSelection = "Scissors"
+   } 
+    
+   playRound (playerSelection, computerSelection)
+   console.log(humanScore)
+   console.log(computerScore)
+   if (humanScore == 5){
+       comments.textContent = "Gameover you win!"
+       humanScore = 0
+       computerScore = 0
+   } else if(computerScore == 5){
+       comments.textContent = "Gameover you lose!"
+       humanScore = 0
+       computerScore = 0
+   }
 
-    playerSelection = prompt("Please enter a choice of either Rock, Paper, or Scissors");
-
-    playRound (playerSelection, computerSelection)
-    console.log(humanScore)
-    console.log(computerScore)
-    if (humanScore == 5){
-        console.log("Gameover you wim!");
-    } else if(computerScore == 5){
-        console.log("Gameover you lose!");
-    }
+    scoreDisplayC.textContent =`Computer : ${computerScore}`
+    scoreDisplayH.textContent =`You : ${humanScore}`
 }
 
-/*
--   Rock-Rock
-Rock-paper
-Rock-Scissors
--   Paper-Paper
-Paper-Rock
-Paper-Scissors
--   Scissors-Scissors
-Scissors-Rock
-Scissors-Paper
+let decisionP = () =>{
+    let playerSelection = 'paper'
+    computerSelection = computerPlay()
 
-*/
+    if (computerSelection == 1) {
+        computerSelection = 'Rock';
+   } else if (computerSelection == 2) {
+       computerSelection = "Paper"
+   } else if (computerSelection == 3) {
+       computerSelection = "Scissors"
+   } 
+    
+   playRound (playerSelection, computerSelection)
+   console.log(humanScore)
+   console.log(computerScore)
+   if (humanScore == 5){
+       comments.textContent = "Gameover you win!"
+       humanScore = 0
+       computerScore = 0
+   } else if(computerScore == 5){
+       comments.textContent = "Gameover you lose!"
+       humanScore = 0
+       computerScore = 0
+   }
+   
+   scoreDisplayC.textContent =`Computer : ${computerScore}`
+   scoreDisplayH.textContent =`You : ${humanScore}`
+   
+}
+let decisionS = () =>{
+    let playerSelection = 'scissors'
+    computerSelection = computerPlay()
+ 
+    if (computerSelection == 1) {
+        computerSelection = 'Rock';
+   } else if (computerSelection == 2) {
+       computerSelection = "Paper"
+   } else if (computerSelection == 3) {
+       computerSelection = "Scissors"
+   } 
+    
+   playRound (playerSelection, computerSelection)
+   console.log(humanScore)
+   console.log(computerScore)
+   if (humanScore == 5){
+      comments.textContent = "Gameover you win!"
+       humanScore = 0
+       computerScore = 0
+   } else if(computerScore == 5){
+    comments.textContent = "Gameover you lose!"
+       humanScore = 0
+       computerScore = 0
+   }
+
+   scoreDisplayC.textContent =`Computer : ${computerScore}`
+   scoreDisplayH.textContent =`You : ${humanScore}`
+    
+}
+
+let comments = document.querySelector('#comments')
+let scoreDisplayC = document.querySelector('#scoreC')
+let scoreDisplayH = document.querySelector('#scoreH')
+scoreDisplayC.textContent =`Computer : ${computerScore}`
+scoreDisplayH.textContent =`You : ${humanScore}`
+comments.textContent = 'Begin!'
+
+
+let humanChoiceR = document.querySelector('#rock-selection')
+let humanChoiceP = document.querySelector('#paper-selection')
+let humanChoiceS = document.querySelector('#scissors-selection')
+
+humanChoiceR.addEventListener('click', decisionR)
+humanChoiceP.addEventListener('click', decisionP)
+humanChoiceS.addEventListener('click', decisionS)
